79fe3e1cd31074d351b464dd283e604dafb0427b
wwwaw <aold88@gmail.com>
2022-03-18 23:44:30 +0300

    slider

---
+++ js/main.js
@@ -0,0 +1,25 @@
+$(function () {
+    $('.top-slider').slick({
+        dots: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                }
+            }
+        ]
+    });
+})
