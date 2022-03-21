eeabb15f041e6b8e6fb3882d3317fd28906edc47
wwwaw <aold88@gmail.com>
2022-03-22 00:03:26 +0300

    2

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ $(function () {
         dots: true,
         slidesToShow: 1,
         slidesToScroll: 1,
+        nextArrow: '<button type="button" class="slick-arrow slick-next"></button>',
+        prevArrow: '<button type="button" class="slick-arrow slick-prev"></button>',
         responsive: [
             {
                 breakpoint: 1024,
